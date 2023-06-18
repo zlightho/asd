06808994a976887e7b29a769168980cbe777a1a2
Artem Mikhaylov <artemmikhaylov6@gmail.com>
2023-06-18 11:03:25 +0300

    bloomfilter

---
+++ bloomfilter.py
@@ -0,0 +1,28 @@
+class BloomFilter:
+    def __init__(self, f_len):
+        self.filter_len = f_len
+        self.bit_array = [0] * f_len
+
+    def hash1(self, str1):
+        # 17
+        code = 0
+        for c in str1:
+            code = (code * 17 + ord(c)) % self.filter_len
+        return code
+
+    def hash2(self, str1):
+        # 223
+        code = 0
+        for c in str1:
+            code = (code * 223 + ord(c)) % self.filter_len
+        return code
+
+    def add(self, str1):
+        # Добавление строки в фильтр
+        index = self.hash1(str1)
+        self.bit_array[index] = 1
+
+    def is_value(self, str1):
+        # Проверка, принадлежит ли строка фильтру
+        index = self.hash1(str1)
+        return self.bit_array[index] == 1
