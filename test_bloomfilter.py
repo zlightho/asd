93f5bc7deaede760f7494935e67406133cd17390
Artem Mikhaylov <artemmikhaylov6@gmail.com>
2023-07-18 18:57:18 +0300

    add tests

--- test_bloomfilter.py
+++ test_bloomfilter.py
@@ -29,36 +29,37 @@ def test_add(self):
 
     self.assertFalse(self.filter_bloom.is_value("not_added_string"))
 
-    def test_false_positive(self):
-        test_strings = [
-            "0123456789",
-            "1234567890",
-            "2345678901",
-            "3456789012",
-            "4567890123",
-            "5678901234",
-            "6789012345",
-            "7890123456",
-            "8901234567",
-            "9012345678",
-        ]
-        non_existing_strings = [
-            "9876543210",
-            "0987654321",
-            "9876543210",
-            "0987654321",
-            "9876543210",
-            "0987654321",
-            "9876543210",
-            "0987654321",
-            "9876543210",
-            "0987654321",
-        ]
-        for string in test_strings:
-            self.filter_bloom.add(string)
 
-        for string in non_existing_strings:
-            self.assertFalse(self.filter_bloom.is_value(string))
+def test_false_positive(self):
+    test_strings = [
+        "0123456789",
+        "1234567890",
+        "2345678901",
+        "3456789012",
+        "4567890123",
+        "5678901234",
+        "6789012345",
+        "7890123456",
+        "8901234567",
+        "9012345678",
+    ]
+    non_existing_strings = [
+        "9876543210",
+        "0987654321",
+        "9876543210",
+        "0987654321",
+        "9876543210",
+        "0987654321",
+        "9876543210",
+        "0987654321",
+        "9876543210",
+        "0987654321",
+    ]
+    for string in test_strings:
+        self.filter_bloom.add(string)
+
+    for string in non_existing_strings:
+        self.assertFalse(self.filter_bloom.is_value(string))
 
 
 if __name__ == "__main__":
