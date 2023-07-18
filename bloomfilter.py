d8b8ed078fb46d7f7be36896562a8090d10ecf66
Artem Mikhaylov <artemmikhaylov6@gmail.com>
2023-07-18 20:45:01 +0300

    autotests review

--- bloomfilter.py
+++ bloomfilter.py
@@ -1,7 +1,7 @@
 class BloomFilter:
     def __init__(self, f_len):
         self.filter_len = f_len
-        self.bit_array = [0] * f_len
+        self.bit_array = 0
 
     def hash1(self, str1):
         code = 0
@@ -18,10 +18,11 @@ class BloomFilter:
     def add(self, str1):
         index1 = self.hash1(str1)
         index2 = self.hash2(str1)
-        self.bit_array[index1] = 1
-        self.bit_array[index2] = 1
+        self.bit_array |= (1 << index1) | (1 << index2)
 
     def is_value(self, str1):
         index1 = self.hash1(str1)
         index2 = self.hash2(str1)
-        return self.bit_array[index1] == 1 or self.bit_array[index2] == 1
+        return (self.bit_array & (1 << index1)) != 0 and (
+            self.bit_array & (1 << index2)
+        ) != 0
