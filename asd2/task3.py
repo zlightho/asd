bcf82a1d790f8d3ced0ebf2f989ee2a3e4b37d5c
Artem Mikhaylov <artemmikhaylov6@gmail.com>
2024-06-04 22:02:19 +0300

    finally

--- asd2/task3.py
+++ asd2/task3.py
@@ -5,8 +5,8 @@ def recursion_list_length(spisok: List[int], length=0) -> int:
     if not spisok:
         return 0
     spisok.pop(0)
-    return 1 + recursion_list_length(spisok, length + 1)
+    return 1 + recursion_list_length(spisok)
 
 
-my_list = [1, 2, 3, 4, 5]
+my_list = [1, 2]
 print(recursion_list_length(my_list))
