dd6a24b936b5bcac10d81cbfa421769a44a7d2f7
Михайлов Артем Константинович <Mikhaylov.A.K@sblogistica.ru>
2024-06-09 14:57:28 +0300

    deque recursion& C:/Users/Mikhaylov.A.K/AppData/Local/Programs/Python/Python311/python.exe c:/Users/Mikhaylov.A.K/Desktop/dev/asd/asd2/task4.py

--- asd2/task4.py
+++ asd2/task4.py
@@ -1,9 +1,56 @@
-def recursion_is_palindrom(string: str) -> bool:
-    left = 0
-    right = len(string) - 1
+class Deque:
+    '''7.1.Мера сложности для операций addHead/removeHead и addTail/removeTail
+    будет различаться из-за разных способов доступа к элементам внутренней
+    структуры данных (в данном случае стандартного списка).
 
-    if left >= right:
-        return True
-    if string[left] != string[right]:
-        return False
-    return recursion_is_palindrom(string[left + 1:right])
+Операции addHead/removeHead работают с началом списка,
+и если внутренний список реализован как динамический массив,
+то добавление и удаление элемента из начала списка может потребовать
+сдвига всех последующих элементов. В этом случае мера сложности будет O(n),
+где n - размер очереди.
+Операции addTail/removeTail работают с концом списка и,
+если внутренний список реализован как динамический массив,
+добавление и удаление элемента из конца списка выполняются эффективно за время O(1).'''
+    def __init__(self):
+        self.deque = []
+
+    def addFront(self, item):
+        self.deque.insert(0, item)
+
+    def addTail(self, item):
+        self.deque.append(item)
+
+    def removeFront(self):
+        if len(self.deque) > 0:
+            return self.deque.pop(0)
+        else:
+            return None
+
+    def removeTail(self):
+        if len(self.deque) > 0:
+            return self.deque.pop()
+        else:
+            return None
+
+    def size(self):
+        return len(self.deque)
+
+def recursion_is_palindrom(string):
+    char_deque = Deque()
+
+    for char in string:
+        char_deque.addTail(char)
+
+    def check_palindrome(deq):
+        if deq.size() <= 1:
+            return True
+        front = deq.removeFront()
+        tail = deq.removeTail()
+        if front != tail:
+            return False
+        return check_palindrome(deq)
+
+    return check_palindrome(char_deque)
+
+my_string = "арозаупаланалапуазора"
+print(recursion_is_palindrom(my_string))
