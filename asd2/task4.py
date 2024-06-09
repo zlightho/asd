e20e1f7ccdff57868347f784813c2fab58b415b5
Михайлов Артем Константинович <Mikhaylov.A.K@sblogistica.ru>
2024-06-09 18:15:28 +0300

    two func palindrome and print_even_numbers

--- asd2/task4.py
+++ asd2/task4.py
@@ -1,8 +1,9 @@
-def is_palindrom(string: str) -> bool:
-    def recursion_is_palindrom(string: str, left: int, right: int) -> bool:
+def recursion_is_palindrom(string: str, left: int, right: int) -> bool:
         if left >= right:
             return True
         if string[left] != string[right]:
             return False
         return recursion_is_palindrom(string, left + 1, right - 1)
+    
+def is_palindrom(string: str) -> bool:
     return recursion_is_palindrom(string, 0, len(string) - 1)
--- asd2/task5.py
+++ asd2/task5.py
@@ -1,7 +1,12 @@
 from typing import List
-def recursive_print_even_values(spisok: List) -> None:
+
+def recursive_print_even_values(spisok: List[int]) -> None:
     if not spisok:
         return
-    if spisok[0] % 2 == 0:
-        print(spisok[0])
-    recursive_print_even_values(spisok[1:])
+    value = spisok.pop(0)
+    if value % 2 == 0:
+        print(value)
+    recursive_print_even_values(spisok)
+
+def print_even_values(spisok: List[int]) -> None:
+    recursive_print_even_values(spisok)
