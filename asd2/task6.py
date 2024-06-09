286d2b22e763ba0143e9d44c9478727fdb7b68e6
Михайлов Артем Константинович <Mikhaylov.A.K@sblogistica.ru>
2024-06-09 18:21:54 +0300

    two func print even indexes

--- asd2/task6.py
+++ asd2/task6.py
@@ -1,8 +1,11 @@
 from typing import List
-def recursive_print_even_indexes(spisok: List, index: int = 0) -> None:
+
+def recursive_print_even_indexes(spisok: List[int], index: int = 0) -> None:
     if index >= len(spisok):
         return
-    if index % 2 == 1:
+    if spisok[index] % 2 == 1:
         print(spisok[index])
-    recursive_print_even_indexes(spisok,index+1)
-    
+    recursive_print_even_indexes(spisok, index + 1)
+
+def print_even_indexes(spisok: List[int]) -> None:
+    recursive_print_even_indexes(spisok)
