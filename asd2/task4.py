8e4412c6848c5a43bf280cb5c384f3037cfd3424
Михайлов Артем Константинович <Mikhaylov.A.K@sblogistica.ru>
2024-06-09 14:06:05 +0300

    redo slice logic

--- asd2/task4.py
+++ asd2/task4.py
@@ -1,11 +1,9 @@
-def recursion_is_palindrom(string: str, left=None, right=None) -> bool:
-    if left is None:
-        left = 0
-    if right is None:
-        right = len(string) - 1
+def recursion_is_palindrom(string: str) -> bool:
+    left = 0
+    right = len(string) - 1
 
     if left >= right:
         return True
     if string[left] != string[right]:
         return False
-    return recursion_is_palindrom(string, left + 1, right - 1)
+    return recursion_is_palindrom(string[left + 1:right])
