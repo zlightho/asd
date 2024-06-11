ababae8ecc84c3db4517e0d09fbd853fa54f2ad2
Artem Mikhaylov <artemmikhaylov6@gmail.com>
2024-06-11 13:23:48 +0300

    delete if cond

--- asd2/task6.py
+++ asd2/task6.py
@@ -1,10 +1,12 @@
 from typing import List
 
+
 def recursive_print_even_indexes(spisok: List[int], index: int) -> None:
     if index >= len(spisok):
         return
     print(spisok[index])
     recursive_print_even_indexes(spisok, index + 2)
 
+
 def print_even_indexes(spisok: List[int]) -> None:
-    recursive_print_even_indexes(spisok,1)
+    recursive_print_even_indexes(spisok, 1)
