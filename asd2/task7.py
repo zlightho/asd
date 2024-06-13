971891e1df8f8f828d9c0d5995e79192e1688a74
Artem Mikhaylov <artemmikhaylov6@gmail.com>
2024-06-13 18:55:07 +0300

    delete typing

--- asd2/task7.py
+++ asd2/task7.py
@@ -24,7 +24,3 @@ def find_second_max(spisok: List):
     elif spisok[0] <= spisok[1]:
         max1, max2 = spisok[1], spisok[0]
     return recursive_find_second_max(spisok, max1, max2, 2)
-
-
-my_list = (1, 2, 4, 5, 6, 6)
-print(find_second_max(my_list))
