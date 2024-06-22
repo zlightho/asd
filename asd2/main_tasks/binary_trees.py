7cd8d675bb26ae549dfb24efb3c71f105e416277
Михайлов Артем Константинович <Mikhaylov.A.K@sblogistica.ru>
2024-06-22 12:05:20 +0300

    rework tests and tuples

--- asd2/main_tasks/binary_trees.py
+++ asd2/main_tasks/binary_trees.py
@@ -1,178 +1,177 @@
-class BSTNode:
+    class BSTNode:
 
-    def __init__(self, key, val, parent):
-        self.NodeKey = key
-        self.NodeValue = val
-        self.Parent = parent
-        self.LeftChild = None
-        self.RightChild = None
-
-
-class BSTFind:
-
-    def __init__(self):
-        self.Node = None
-
-        self.NodeHasKey = False
-        self.ToLeft = False
+        def __init__(self, key, val, parent):
+            self.NodeKey = key
+            self.NodeValue = val
+            self.Parent = parent
+            self.LeftChild = None
+            self.RightChild = None
 
 
-class BST:
+    class BSTFind:
 
-    def __init__(self, node):
-        self.Root = node
+        def __init__(self):
+            self.Node = None
 
-    def FindNodeByKey(self, key):
-        result = BSTFind()
-        current = self.Root
+            self.NodeHasKey = False
+            self.ToLeft = False
 
-        while current is not None:
-            if current.NodeKey == key:
-                result.Node = current
-                result.NodeHasKey = True
-                return result
-            elif key < current.NodeKey:
-                if current.LeftChild is None:
+
+    class BST:
+
+        def __init__(self, node):
+            self.Root = node
+
+        def FindNodeByKey(self, key):
+            result = BSTFind()
+            current = self.Root
+
+            while current is not None:
+                if current.NodeKey == key:
                     result.Node = current
-                    result.ToLeft = True
-                    return result
-                current = current.LeftChild
-            else:
-                if current.RightChild is None:
-                    result.Node = current
-                    return result
-                current = current.RightChild
-
-        return result
-    
-    def AddKeyValue(self, key, val):
-        find_result = self.FindNodeByKey(key)
-
-        if find_result.NodeHasKey:
-            return False
-
-        new_node = BSTNode(key, val, find_result.Node)
-
-        if find_result.Node is None:
-            self.Root = new_node
-        else:
-            if find_result.ToLeft:
-                find_result.Node.LeftChild = new_node
-            else:
-                find_result.Node.RightChild = new_node
-
-        return True
-
-    def FinMinMax(self, FromNode, FindMax):
-        current = FromNode
-        if FindMax:
-            while current.RightChild is not None:
-                current = current.RightChild
-        else:
-            while current.LeftChild is not None:
-                current = current.LeftChild
-        return current
-
-    def DeleteNodeByKey(self, key):
-        find_result = self.FindNodeByKey(key)
-
-        if not find_result.NodeHasKey:
-            return False
-
-        node_to_delete = find_result.Node
-
-        if node_to_delete.LeftChild is None and node_to_delete.RightChild is None:
-            if node_to_delete == self.Root:
-                self.Root = None
-            else:
-                if node_to_delete.Parent.LeftChild == node_to_delete:
-                    node_to_delete.Parent.LeftChild = None
+                    result.NodeHasKey = True
+                elif key < current.NodeKey:
+                    if current.LeftChild is None:
+                        result.Node = current
+                        result.ToLeft = True
+                        return result
+                    current = current.LeftChild
                 else:
-                    node_to_delete.Parent.RightChild = None
+                    if current.RightChild is None:
+                        result.Node = current
+                        return result
+                    current = current.RightChild
 
-        elif node_to_delete.LeftChild is None or node_to_delete.RightChild is None:
-            if node_to_delete.LeftChild is not None:
-                child = node_to_delete.LeftChild
-            else:
-                child = node_to_delete.RightChild
+            return result
+        
+        def AddKeyValue(self, key, val):
+            find_result = self.FindNodeByKey(key)
 
-            if node_to_delete == self.Root:
-                self.Root = child
+            if find_result.NodeHasKey:
+                return False
+
+            new_node = BSTNode(key, val, find_result.Node)
+
+            if find_result.Node is None:
+                self.Root = new_node
             else:
-                if node_to_delete.Parent.LeftChild == node_to_delete:
-                    node_to_delete.Parent.LeftChild = child
+                if find_result.ToLeft:
+                    find_result.Node.LeftChild = new_node
                 else:
-                    node_to_delete.Parent.RightChild = child
-            child.Parent = node_to_delete.Parent
+                    find_result.Node.RightChild = new_node
 
-        else:
-            successor = self.FinMinMax(node_to_delete.RightChild, False)
-            node_to_delete.NodeKey = successor.NodeKey
-            node_to_delete.NodeValue = successor.NodeValue
-            if successor.Parent.LeftChild == successor:
-                successor.Parent.LeftChild = successor.RightChild
+            return True
+
+        def FinMinMax(self, FromNode, FindMax):
+            current = FromNode
+            if FindMax:
+                while current.RightChild is not None:
+                    current = current.RightChild
             else:
-                successor.Parent.RightChild = successor.RightChild
-            if successor.RightChild is not None:
-                successor.RightChild.Parent = successor.Parent
+                while current.LeftChild is not None:
+                    current = current.LeftChild
+            return current
 
-        return True
+        def DeleteNodeByKey(self, key):
+            find_result = self.FindNodeByKey(key)
 
-    def Count(self):
-        return self._CountRecursive(self.Root)
+            if not find_result.NodeHasKey:
+                return False
 
-    def _CountRecursive(self, node):
-        if node is None:
-            return 0
-        return (
-            1
-            + self._CountRecursive(node.LeftChild)
-            + self._CountRecursive(node.RightChild)
-        )
-        
-    def WideAllNodes(self):
-        if self.Root is None:
-            return []
-        
-        queue = [self.Root]
-        result = []
-        
-        while queue:
-            node = queue.pop(0)
-            result.append(node)
-            if node.LeftChild is not None:
-                queue.append(node.LeftChild)
-            if node.RightChild is not None:
-                queue.append(node.RightChild)
-        
-        return result
-    
-    def DeepAllNodes(self, order):
-        result = []
+            node_to_delete = find_result.Node
 
-        def in_order(node):
-            if node is not None:
-                in_order(node.LeftChild)
-                result.append((node,))
-                in_order(node.RightChild)
+            if node_to_delete.LeftChild is None and node_to_delete.RightChild is None:
+                if node_to_delete == self.Root:
+                    self.Root = None
+                else:
+                    if node_to_delete.Parent.LeftChild == node_to_delete:
+                        node_to_delete.Parent.LeftChild = None
+                    else:
+                        node_to_delete.Parent.RightChild = None
 
-        def post_order(node):
-            if node is not None:
-                post_order(node.LeftChild)
-                post_order(node.RightChild)
-                result.append((node,))
+            elif node_to_delete.LeftChild is None or node_to_delete.RightChild is None:
+                if node_to_delete.LeftChild is not None:
+                    child = node_to_delete.LeftChild
+                else:
+                    child = node_to_delete.RightChild
 
-        def pre_order(node):
-            if node is not None:
-                result.append((node,))
-                pre_order(node.LeftChild)
-                pre_order(node.RightChild)
+                if node_to_delete == self.Root:
+                    self.Root = child
+                else:
+                    if node_to_delete.Parent.LeftChild == node_to_delete:
+                        node_to_delete.Parent.LeftChild = child
+                    else:
+                        node_to_delete.Parent.RightChild = child
+                child.Parent = node_to_delete.Parent
 
-        if order == 0:
-            in_order(self.Root)
-        elif order == 1:
-            post_order(self.Root)
-        elif order == 2:
-            pre_order(self.Root)
+            else:
+                successor = self.FinMinMax(node_to_delete.RightChild, False)
+                node_to_delete.NodeKey = successor.NodeKey
+                node_to_delete.NodeValue = successor.NodeValue
+                if successor.Parent.LeftChild == successor:
+                    successor.Parent.LeftChild = successor.RightChild
+                else:
+                    successor.Parent.RightChild = successor.RightChild
+                if successor.RightChild is not None:
+                    successor.RightChild.Parent = successor.Parent
 
-        return result
+            return True
+
+        def Count(self):
+            return self._CountRecursive(self.Root)
+
+        def _CountRecursive(self, node):
+            if node is None:
+                return 0
+            return (
+                1
+                + self._CountRecursive(node.LeftChild)
+                + self._CountRecursive(node.RightChild)
+            )
+            
+        def WideAllNodes(self):
+            if self.Root is None:
+                return []
+
+            queue = [self.Root]
+            result = []
+
+            while queue:
+                node = queue.pop(0)
+                result.append(node)
+                if node.LeftChild is not None:
+                    queue.append(node.LeftChild)
+                if node.RightChild is not None:
+                    queue.append(node.RightChild)
+
+            return result
+
+        def DeepAllNodes(self, order):
+            result = []
+
+            def in_order(node):
+                if node is not None:
+                    in_order(node.LeftChild)
+                    result.append(node)
+                    in_order(node.RightChild)
+
+            def post_order(node):
+                if node is not None:
+                    post_order(node.LeftChild)
+                    post_order(node.RightChild)
+                    result.append(node)
+
+            def pre_order(node):
+                if node is not None:
+                    result.append(node)
+                    pre_order(node.LeftChild)
+                    pre_order(node.RightChild)
+
+            if order == 0:
+                in_order(self.Root)
+            elif order == 1:
+                post_order(self.Root)
+            elif order == 2:
+                pre_order(self.Root)
+
+            return result
--- asd2/main_tasks/tests/tests_binary_trees.py
+++ asd2/main_tasks/tests/tests_binary_trees.py
@@ -98,11 +98,11 @@ class TestBST(unittest.TestCase):
         self.tree.AddKeyValue(20, 'right-right')
 
         wide_nodes = self.tree.WideAllNodes()
-        keys = [node.NodeKey for node in wide_nodes]
+        keys = [node[0].NodeKey for node in wide_nodes]
         expected_keys = [10, 5, 15, 2, 7, 12, 20]
 
         self.assertEqual(keys, expected_keys)
-    
+
     def test_DeepAllNodes_in_order(self):
         self.tree.AddKeyValue(5, 'left')
         self.tree.AddKeyValue(15, 'right')
@@ -112,7 +112,9 @@ class TestBST(unittest.TestCase):
         self.tree.AddKeyValue(20, 'right-right')
 
         deep_nodes = self.tree.DeepAllNodes(0)
-        self.assertEqual([node[0].NodeKey for node in deep_nodes], [2, 5, 7, 10, 12, 15, 20])
+        keys = [node[0].NodeKey for node in deep_nodes]
+        expected_keys = [2, 5, 7, 10, 12, 15, 20]
+        self.assertEqual(keys, expected_keys)
 
     def test_DeepAllNodes_post_order(self):
         self.tree.AddKeyValue(5, 'left')
@@ -123,7 +125,10 @@ class TestBST(unittest.TestCase):
         self.tree.AddKeyValue(20, 'right-right')
 
         deep_nodes = self.tree.DeepAllNodes(1)
-        self.assertEqual([node[0].NodeKey for node in deep_nodes], [2, 7, 5, 12, 20, 15, 10])
+        keys = [node[0].NodeKey for node in deep_nodes]
+        expected_keys = [2, 7, 5, 12, 20, 15, 10]
+        
+        self.assertEqual(keys, expected_keys)
 
     def test_DeepAllNodes_pre_order(self):
         self.tree.AddKeyValue(5, 'left')
@@ -134,7 +139,10 @@ class TestBST(unittest.TestCase):
         self.tree.AddKeyValue(20, 'right-right')
 
         deep_nodes = self.tree.DeepAllNodes(2)
-        self.assertEqual([node[0].NodeKey for node in deep_nodes], [10, 5, 2, 7, 15, 12, 20])
+        keys = [node[0].NodeKey for node in deep_nodes]
+        expected_keys = [10, 5, 2, 7, 15, 12, 20]
+        
+        self.assertEqual(keys, expected_keys)
 
 if __name__ == "__main__":
-    unittest.main()
+    unittest.main() 
