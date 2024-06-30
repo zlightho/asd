d50efd21d272efe6ca989170acaa81c720f6c9ba
Artem Mikhaylov <artemmikhaylov6@gmail.com>
2024-06-30 09:27:31 +0300

    bbstarr

--- asd2/main_tasks/bbstarray.py
+++ asd2/main_tasks/bbstarray.py
@@ -1,8 +1,6 @@
 def GenerateBBSTArray(a):
-    # Сортируем входной массив
     a = sorted(a)
 
-    # Начальная длина дерева
     tree_size = 2 ** (len(a).bit_length()) - 1
     array_bst = [None] * tree_size
 
@@ -12,12 +10,8 @@ def GenerateBBSTArray(a):
 
 def _binary_sort(a, tree, index):
     if not a:
-        return
-    # Центрированный элемент текущего массива
+        return None
     mid = len(a) // 2
-    # Помещаем центрированный элемент в дерево
     tree[index] = a[mid]
-
-    # Рекурсивно заполняем левую и правую часть дерева
     _binary_sort(a[:mid], tree, 2 * index + 1)
     _binary_sort(a[mid + 1 :], tree, 2 * index + 2)
